632afe67934d28f0b814fbb80f94ff69be6dd0f8
UQUKO <145220741+UQUKO@users.noreply.github.com>
2024-03-22 02:06:39 +0200

    Initializing the project

---
+++ src/test/java/org/example/fujitsufooddelivery/FujitsuFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fujitsufooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FujitsuFoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
