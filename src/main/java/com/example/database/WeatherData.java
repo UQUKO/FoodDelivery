858479a3349cc81a073e18d3004578453fc74024
UQUKO <145220741+UQUKO@users.noreply.github.com>
2024-03-30 02:58:58 +0200

    Small deletions of unused/unimportant stuff

--- src/main/java/com/example/database/WeatherData.java
+++ src/main/java/com/example/database/WeatherData.java
@@ -30,61 +30,29 @@ public class WeatherData {
     @Column(name = "windspeed")
     private double windSpeed;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getObservationTime() {
-        return observationTime;
-    }
 
     public void setObservationTime(LocalDateTime observationTime) {
         this.observationTime = observationTime;
-    }
-
-    public String getStation() {
-        return station;
     }
 
     public void setStation(String station) {
         this.station = station;
     }
 
-    public int getWmocode() {
-        return wmocode;
-    }
-
     public void setWmocode(int wmocode) {
         this.wmocode = wmocode;
-    }
-
-    public String getPhenomenon() {
-        return phenomenon;
     }
 
     public void setPhenomenon(String phenomenon) {
         this.phenomenon = phenomenon;
     }
 
-    public double getAirTemperature() {
-        return airTemperature;
-    }
-
     public void setAirTemperature(double airTemperature) {
         this.airTemperature = airTemperature;
-    }
-
-    public double getWindSpeed() {
-        return windSpeed;
     }
 
     public void setWindSpeed(double windSpeed) {
         this.windSpeed = windSpeed;
     }
 
-    // Constructors, getters, and setters
 }
